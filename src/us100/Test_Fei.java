a40019251615a48eac0eba44b3bdd98a010d5925
Fei Zhou <sophia08241@gmail.com>
2022-01-24 16:10:29 -0500

    Second Commit From Fei

--- src/us100/Test_Fei.java
+++ src/us100/Test_Fei.java
@@ -4,5 +4,6 @@ public class Test_Fei {
 
     public static void main(String[] args) {
         System.out.println("Hello Guys, This is Fei");
+        System.out.println("How are you today ");
     }
 }
